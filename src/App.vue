56443220466db6ce5d8e8998dabac50b1f79963b
Adam Hutson <hutson.adam@yahoo.com>
2019-01-24 20:14:40 +0000

    vue files from set up practice

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <input
+      v-model="message"
+      type="text">
+      <h2 class="message">{{ message }}</h2>
+      <h3>One More Message</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
